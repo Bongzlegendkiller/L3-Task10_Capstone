7b8b3750726befdee19b0df488e340a2c648a296
Bongani Cacambile <b.cacambile1@gmail.com>
2023-10-17 11:06:31 +0200

    added docs string in portfolio views.py

--- mysite/portfolio/views.py
+++ mysite/portfolio/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def bongani_portfolio(request):
+
+    """
+    Request bongani_portfolio
+    
+    :param: Request object
+    :type: HttpRequest
+
+    :return: bongani.html
+    :rtype: HttpResponse
+
+    """
     return render(request,'bongani.html')
 
 
